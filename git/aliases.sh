99113f74427aa3268f5b77354d06dab7df27ebf6
Peter Toth <tothpeter08@gmail.com>
2023-09-01 14:53:01 +0300

    Facelift Git alises

--- git/aliases.sh
+++ git/aliases.sh
@@ -7,31 +7,32 @@ alias grbi7='grbi HEAD~7'
 alias grbi8='grbi HEAD~8'
 alias grbi9='grbi HEAD~9'
 
-# Git rebase master
+# Git rebase master (It stashes the changes if there were any)
 alias grbm='git_rebase_to_main_with_changes'
 
 # Git rebase master iterative
 alias grbmi='EDITOR="code --wait" grbi main'
 
-# Git update
-alias gupd='gplm ; grbm'
+# Git update = sync down changes from the remote master to the current branch
+alias gupd='gplm ; grbm ; gfa'
 # Git update iterative
 alias gupdi='gplm && grbmi'
 # Git update remote
-alias gupdr='gplm ; grbm ; gpf'
+alias gupdr='gplm ; grbm ; gpf ; gfa'
 
-alias gc='git commit -m'
-alias gam='git add . ; git commit --amend --no-edit'
+alias gc='git commit -m' # git commit for lazy devs like myself
+alias gam='git add . ; git commit --amend --no-edit' # ammend all changes to the last commit
 
 alias gl='git log --oneline --graph --all --decorate'
-alias gr='git remote -v'
+alias gr='git remote -v' # lists the remotes
 alias gs='git status'
 alias gpf='git push -f'
 alias gpl='git pull'
 alias gplr='git pull --rebase'
-alias gplm='git fetch origin $(git_main_branch):$(git_main_branch)' # pulls the main branch without checking it out
+# Git pull master = update the local main branch, without the need to switch to it
+alias gplm='git fetch origin $(git_main_branch):$(git_main_branch)'
 alias nah='git reset --hard && git clean -df' # discard all changes
-alias gu="git reset --soft 'HEAD^'" # undo last commit and keep its changes
+alias gu="git reset --soft 'HEAD^'" # Git undo = removes the last commit while keeping its changes
 
 alias gitx='open -a SourceTree $(git rev-parse --show-toplevel || echo .)'
 alias gx='gitx'
