5ef71c2900e7fc76d3dc68c26e7284fbc77d2d1c
Peter Toth <tothpeter08@gmail.com>
2023-10-19 19:19:52 +0400

    Add Kubernetes aliases and create a standalone file for other aliases

--- others/aliases.sh
+++ others/aliases.sh
@@ -1,1 +1,7 @@
 alias backup_vlc_config='mv -v ~/Library/Preferences/org.videolan.vlc/vlcrc ~/.dotfiles/others/vlc/vlcrc && ln -s ~/.dotfiles/others/vlc/vlcrc ~/Library/Preferences/org.videolan.vlc/vlcrc'
+
+alias k='kubectl'
+alias kl='kubectl logs -f'
+alias kg='kubectl get'
+alias kp='kubectl get pods'
+alias k_pods_mine='kubectl get pods | grep peter'
