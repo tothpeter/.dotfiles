3cd069e4ebdab49eb2084c81d9d4f8c1860eed80
Peter Toth <tothpeter08@gmail.com>
2023-10-25 22:13:15 +0400

    Add shortcut to restore VLC config from dotfiles

--- others/aliases.sh
+++ others/aliases.sh
@@ -1,4 +1,5 @@
 alias backup_vlc_config='mv -v ~/Library/Preferences/org.videolan.vlc/vlcrc ~/.dotfiles/others/vlc/vlcrc && ln -s ~/.dotfiles/others/vlc/vlcrc ~/Library/Preferences/org.videolan.vlc/vlcrc'
+alias apply_vlc_config_from_dotfiles='rmd ~/Library/Preferences/org.videolan.vlc/vlcrc ; ln -s ~/.dotfiles/others/vlc/vlcrc ~/Library/Preferences/org.videolan.vlc/vlcrc'
 
 alias k='kubectl'
 alias kl='kubectl logs -f'
