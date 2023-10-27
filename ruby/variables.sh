212b2ceaa0e23f8b086edb8233fbf22578bbee8f
Peter Toth <tothpeter08@gmail.com>
2023-10-27 14:56:04 +0400

    Add hack to be able to use the PG gem without having PostgreSQL installed

---
+++ ruby/variables.sh
@@ -0,0 +1,4 @@
+# Hack to be able to use the PG gem without having PostgreSQL installed
+export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/libpq/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/libpq/include"
