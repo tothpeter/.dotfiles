fa66377ec997611143afcf0f72583d0c1f8ed55a
Peter Toth <tothpeter08@gmail.com>
2024-06-20 01:16:34 +0400

    Put only one * in the Git part of the ZSH prompt when there is any change to commit

--- zsh/widgets/prompt.zsh
+++ zsh/widgets/prompt.zsh
@@ -6,13 +6,13 @@ zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' get-revision true
 zstyle ':vcs_info:*' unstagedstr '*'
 zstyle ':vcs_info:*' stagedstr '*'
-zstyle ':vcs_info:git:*' formats '%b%u%c%m '
-zstyle ':vcs_info:git:*' actionformats '%12.12i|%a%u%c%m '
-zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
+zstyle ':vcs_info:git:*' formats '%b%m '
+zstyle ':vcs_info:git:*' actionformats '%12.12i|%a%m '
+zstyle ':vcs_info:git*+set-message:*' hooks git-changes
 
-+vi-git-untracked() {
++vi-git-changes() {
   if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
-     git status --porcelain | grep -m 1 '^??' &>/dev/null
+     git status --porcelain | grep -E -q '^ ?(M|A|D|R|C|\?)' &>/dev/null
   then
     hook_com[misc]='*'
   fi
