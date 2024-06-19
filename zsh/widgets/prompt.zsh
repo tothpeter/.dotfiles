e5785d04a438946bd3c538e4fcc4b146740445ed
Peter Toth <tothpeter08@gmail.com>
2024-06-19 22:30:37 +0400

    Customize my ZSH prompt

---
+++ zsh/widgets/prompt.zsh
@@ -0,0 +1,20 @@
+autoload -Uz vcs_info
+precmd() { vcs_info }
+
+# Define format for Git
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' unstagedstr '*'
+zstyle ':vcs_info:*' stagedstr '*'
+zstyle ':vcs_info:git:*' formats '%b%u%c%m '
+zstyle ':vcs_info:git:*' actionformats '%b|%a%m%u%c '
+zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
+
++vi-git-untracked() {
+  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
+     git status --porcelain | grep -m 1 '^??' &>/dev/null
+  then
+    hook_com[misc]='*'
+  fi
+}
+
+PROMPT='%F{blue}%1~%f ${vcs_info_msg_0_}❯ '
