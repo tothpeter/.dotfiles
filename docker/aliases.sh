070ccf0fe9005e4b2b953b704345b81ab7c97abd
Peter Toth <tothpeter08@gmail.com>
2024-07-10 01:09:12 +0400

    Add Docker related aliases

--- docker/aliases.sh
+++ docker/aliases.sh
@@ -1,2 +1,9 @@
+# Docker Nukem to remove all containers, images, volumes, and networks
+alias d_nukem='docker system prune --all --volumes -f'
+
 alias de='docker exec -it `docker ps -aqf "name=$(basename $(pwd))-web"`'
 alias con='de /bin/bash'
+
+alias up="docker-compose up -d"
+alias upb="docker-compose up --build -d"
+alias down="docker-compose down"
