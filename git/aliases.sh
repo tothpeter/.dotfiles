c7c90a22a55efa6cf7c337e218b7d6e61ec86369
Peter Toth <tothpeter08@gmail.com>
2024-08-07 21:40:49 +0400

    Prune and cleanup Git aliases and functions

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,3 +1,4 @@
+# Rebase interactively the last n commits
 alias grbi2='grbi HEAD~2 --autostash'
 alias grbi3='grbi HEAD~3 --autostash'
 alias grbi4='grbi HEAD~4 --autostash'
@@ -7,75 +8,74 @@ alias grbi7='grbi HEAD~7 --autostash'
 alias grbi8='grbi HEAD~8 --autostash'
 alias grbi9='grbi HEAD~9 --autostash'
 
-# Git rebase master
+# Rebase master
 alias grbm='git rebase $(git_main_branch) --autostash'
 
 # Squash fixup commits
 alias grb_fixup='git rebase -i --autosquash $(git_main_branch)'
 
-# Rebase all commits in the current feature branch iteratively
+# Rebase interactive all - Rebase all commits of the current feature branch interactively
 alias grbia='git rebase -i $(git_main_branch) --autostash'
-# Git rebase continue all - continue the rebase without editing the commit message
+# Rebase continue all - Continue the rebase without editing the commit message
 alias grbca='git add --all && GIT_EDITOR=true git rebase --continue'
 
 alias git_clean_branches='git_clean_not_my_branches && git_clean_merged_branches'
 alias git_clean_not_my_branches='git branch --format "%(refname:short)" | grep -v "peter-" | grep -v "$(git_main_branch)" | xargs -r git branch -D'
 alias git_clean_merged_branches="git branch --format '%(refname:short) %(upstream:track)' | awk '\$2 == \"[gone]\" { print \$1 }' | xargs -r git branch -D"
 
-# Git prune
-alias gpr='gfa && git_clean_branches'
-
-# Update = sync changes from the remote master to the current branch
+# Update - Sync changes from the remote master to the current branch
 alias gupd='gplm && grbm && git_clean_merged_branches && gfa'
-# Update remote = sync changes from the current branch to its remote counterpart
+# Update remote - Sync changes from the remote master to the current branch and push the changes
 alias gupdr='gupd && gpf'
 
 # Commit for lazy devs like myself
 alias gc='git add . && git commit -m'
-# Fixup commit | brew install fzf
+# Fixup commit
 alias gc_fixup="git log -n 50 --pretty=format:'%h %s' --no-merges | fzf | awk \"{ print \$1 }\" | xargs -o git commit --fixup"
-# Ammend all changes to the last commit
+# Ammend - Ammend all changes to the last commit
 alias gam='git add . && git commit --amend --no-edit'
 
-# Git log
+# Log
 alias gl='git log --oneline --graph'
-# Git log all
+# Log all
 alias gla='git log --oneline --graph --all --decorate'
-# List the remotes
+# Remotes - List all remotes
 alias gr='git remote -v'
 alias gs='git status'
 alias gpf='git push -f'
 alias gpl='git pull'
+# Pull rebase
 alias gplr='git pull --rebase origin $(git_current_branch)'
-# Pull force = overwrite local branch with its remote counterpart
+# Pull force - Overwrite the local branch with its remote counterpart
 alias gplf='git reset origin/$(git_current_branch) --hard'
-# Pull master = update the local main branch, without the need to switch to it
+# Pull master - Update the local main branch without switching branches
 alias gplm='git fetch origin $(git_main_branch):$(git_main_branch) --prune'
 # Discard all changes
 alias nah='git reset --hard && git clean -df'
-# Undo = removes the last commit while keeping its changes
+# Undo - Removes the last commit while keeping its changes
 alias gu="git reset --soft 'HEAD^'"
 
-# Checkout back to the previous branch
-alias gchb="git checkout -"
-
-# Diff origin
-alias gdo="git diff @{upstream}"
+# Checkout back
+alias gcb="git checkout -"
 
 # GitX: Open repository in SourceTree (don't ask why I associate GitX with SourceTree)
 alias gx='open -a SourceTree $(git rev-parse --show-toplevel || echo .)'
-alias gxc='open -a SourceTree ~/.dotfiles' # GitX config local
-alias gxcl='open -a SourceTree ~/.dotfiles/local' # GitX config local
-alias gxl='lazygit'
+ # GitX config
+alias gxc='open -a SourceTree ~/.dotfiles'
+ # GitX config local
+alias gxcl='open -a SourceTree ~/.dotfiles/local'
+alias gxl='gxcl'
 
-alias gpc='git -C ~/.dotfiles push' # Git push config
-alias gpcl='git -C ~/.dotfiles/local push' # Git push config local
+# Push config
+alias gpc='git -C ~/.dotfiles push'
+# Push config local
+alias gpcl='git -C ~/.dotfiles/local push'
 
-# Git bisect start
+# Bisect start
 alias gbss="gbs_start"
-# Git bisect abort
+# Bisect abort
 alias gbsa="git bisect reset"
-# Git bisect good
+# Bisect good
 alias gbsg="git bisect old"
-# Git bisect bad
+# Bisect bad
 alias gbsb="git bisect new"
--- git/functions.sh
+++ git/functions.sh
@@ -1,11 +1,11 @@
-# grbi - Git rebase interactive
+# Rebase interactive
 #
 # Usage:
 #   grbi <number> - Starts an interactive rebase for the last <n> commits
 #   grbi <args>   - Calls 'git rebase -i' with the provided arguments
 #
 # If the first argument is a number, it rebases interactively the last <n> commits.
-# Otherwise, it passes all arguments to 'git rebase -i' for standard behavior.
+# Otherwise, it falls back to the original behavior.
 
 unalias grbi
 grbi() {
@@ -16,22 +16,20 @@ grbi() {
   fi
 }
 
-# Git diff remote
-# This filters out the changes that other contributors have made that
-# I don't care about! Which comes handy after a hairy rebase.
+# Diff remote
+#
+# Compare the current branch with its remote counterpart after fixing conflicts.
+# This filters out files that I didn't, but others changed.
 gdr() {
   local current_branch=$(git_current_branch)
   git diff origin/$current_branch $current_branch -- $(git diff master $current_branch --name-only)
 }
 
-# Git bisect start - Start a bisect session
+# Bisect start - Start a bisect session
 #
 # Usage:
-#   gbs_start [target_commit]
-#
-# Arguments:
-#   target_commit - The left boundary of the bisect session. If not provided,
-#                   a commit is selected interactively.
+#   gbs_start          - Start a bisect session with the commit selected interactively
+#   gbs_start <commit> - Start a bisect session with the provided commit
 gbs_start() {
   if [ -n "$1" ]; then
     local -r target_commit="$1"
@@ -44,19 +42,27 @@ gbs_start() {
   git bisect new
 }
 
-# Git checkout
+# Checkout
+#
+# Usage:
+#   gco          - List all the branches and let the user select one
+#   gco <branch> - Checkout the provided branch
 unalias gco
 gco() {
   if [ -z "$1" ]; then
     # List all the branches and let the user select one if no arguments are provided
     git branch | grep -v "^\*" | fzf --height=20% --reverse --info=inline | xargs git checkout
   else
-    # Otherwise, fallback to the default behavior
+    # Otherwise, fallback to the original behavior
     git checkout $@
   fi
 }
 
-# Git cherry-pick
+# Cherry-pick
+#
+# Usage:
+#   gcp          - List all the branches and let the user select one
+#   gcp <commit> - Cherry-pick the provided commit
 unalias gcp
 gcp() {
   if [ -z "$1" ]; then
@@ -66,7 +72,7 @@ gcp() {
       grep -o "[a-f0-9]\{7\}" | sed -n "1p" | \
       xargs git cherry-pick
   else
-    # Otherwise, fallback to the default behavior
+    # Otherwise, fallback to the original behavior
     git cherry-pick $@
   fi
 }
