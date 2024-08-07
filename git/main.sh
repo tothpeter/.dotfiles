e5ab4b9c5096ec2dac28db6ea3868f869fba2582
Peter Toth <tothpeter08@gmail.com>
2024-08-07 21:55:39 +0400

    Merge Git aliases and functions into one file

--- git/main.sh
+++ git/main.sh
@@ -1,3 +1,21 @@
+# Rebase interactive
+#
+# Usage:
+#   grbi <number> - Starts an interactive rebase for the last <n> commits
+#   grbi <args>   - Calls 'git rebase -i' with the provided arguments
+#
+# If the first argument is a number, it rebases interactively the last <n> commits.
+# Otherwise, it falls back to the original behavior.
+
+unalias grbi
+grbi() {
+  if [[ $1 =~ ^[0-9]+$ ]]; then
+    grbi "HEAD~$1"
+  else
+    git rebase -i $@
+  fi
+}
+
 # Rebase interactively the last n commits
 alias grbi2='grbi HEAD~2 --autostash'
 alias grbi3='grbi HEAD~3 --autostash'
@@ -8,14 +26,15 @@ alias grbi7='grbi HEAD~7 --autostash'
 alias grbi8='grbi HEAD~8 --autostash'
 alias grbi9='grbi HEAD~9 --autostash'
 
+# Rebase interactive all - Rebase all commits of the current feature branch interactively
+alias grbia='git rebase -i $(git_main_branch) --autostash'
+
 # Rebase master
 alias grbm='git rebase $(git_main_branch) --autostash'
 
 # Squash fixup commits
 alias grb_fixup='git rebase -i --autosquash $(git_main_branch)'
 
-# Rebase interactive all - Rebase all commits of the current feature branch interactively
-alias grbia='git rebase -i $(git_main_branch) --autostash'
 # Rebase continue all - Continue the rebase without editing the commit message
 alias grbca='git add --all && GIT_EDITOR=true git rebase --continue'
 
@@ -55,9 +74,6 @@ alias nah='git reset --hard && git clean -df'
 # Undo - Removes the last commit while keeping its changes
 alias gu="git reset --soft 'HEAD^'"
 
-# Checkout back
-alias gcb="git checkout -"
-
 # GitX: Open repository in SourceTree (don't ask why I associate GitX with SourceTree)
 alias gx='open -a SourceTree $(git rev-parse --show-toplevel || echo .)'
  # GitX config
@@ -79,3 +95,68 @@ alias gbsa="git bisect reset"
 alias gbsg="git bisect old"
 # Bisect bad
 alias gbsb="git bisect new"
+
+
+# Diff remote
+#
+# Compare the current branch with its remote counterpart after fixing conflicts.
+# This filters out files that I didn't, but others changed.
+gdr() {
+  local current_branch=$(git_current_branch)
+  git diff origin/$current_branch $current_branch -- $(git diff master $current_branch --name-only)
+}
+
+# Bisect start - Start a bisect session
+#
+# Usage:
+#   gbs_start          - Start a bisect session with the commit selected interactively
+#   gbs_start <commit> - Start a bisect session with the provided commit
+gbs_start() {
+  if [ -n "$1" ]; then
+    local -r target_commit="$1"
+  else
+    local -r target_commit=$(git log -n 100 --oneline | fzf | awk '{print $1}')
+  fi
+
+  git bisect start
+  git bisect old "$target_commit"
+  git bisect new
+}
+
+# Checkout
+#
+# Usage:
+#   gco          - List all the branches and let the user select one
+#   gco <branch> - Checkout the provided branch
+unalias gco
+gco() {
+  if [ -z "$1" ]; then
+    # List all the branches and let the user select one if no arguments are provided
+    git branch | grep -v "^\*" | fzf --height=20% --reverse --info=inline | xargs git checkout
+  else
+    # Otherwise, fallback to the original behavior
+    git checkout $@
+  fi
+}
+
+# Checkout back
+alias gcb="git checkout -"
+
+# Cherry-pick
+#
+# Usage:
+#   gcp          - List all the branches and let the user select one
+#   gcp <commit> - Cherry-pick the provided commit
+unalias gcp
+gcp() {
+  if [ -z "$1" ]; then
+    # List all the branches and let the user select one if no arguments are provided
+    git log -n 200 --oneline --graph --all | \
+      fzf --height=20% --reverse --info=inline | \
+      grep -o "[a-f0-9]\{7\}" | sed -n "1p" | \
+      xargs git cherry-pick
+  else
+    # Otherwise, fallback to the original behavior
+    git cherry-pick $@
+  fi
+}
