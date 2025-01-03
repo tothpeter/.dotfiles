d8d655a648e8aa99c21a46ededda19b34b9bce67
Peter Toth <tothpeter08@gmail.com>
2025-01-04 00:27:58 +0700

    Change Git info in the terminal prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -6,7 +6,7 @@ precmd() { vcs_info }
 # Define format for Git
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' get-revision true
-zstyle ':vcs_info:git:*' formats '[%b%m] '
+zstyle ':vcs_info:git:*' formats '%b%f%m '
 zstyle ':vcs_info:git:*' actionformats '%a%m '
 zstyle ':vcs_info:git*+set-message:*' hooks git-changes
 
@@ -32,8 +32,8 @@ zstyle ':vcs_info:git*+set-message:*' hooks git-changes
 
   # Do we have any changes?
   if git status --porcelain | grep -E -q '^ ?(M|A|D|R|C|\?)' &>/dev/null; then
-    hook_com[misc]='*' # Add star after the branch name if there are changes
+    hook_com[misc]='%F{white}!' # Add ! after the branch name if there are changes
   fi
 }
 
-PROMPT='%F{blue}%1~%f %F{red}${vcs_info_msg_0_}%f❯ '
+PROMPT='%F{blue}%1~%f %F{241}${vcs_info_msg_0_}%f❯ '
